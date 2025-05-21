1d77b3fa5459f2d345ec3ed5ebb5489fb04d895b
akbar <moakbar202@gmail.com>
2025-05-21 23:32:09 +0100

    authorisation component now works

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,11 +1,41 @@
 import { Navigate } from "react-router-dom";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 /**
  * this just checks if the user has a token  and then lets them through, 
  * if not it would
  */
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-    const token = localStorage.getItem("token"); // or check auth context
-    return token ? children : <Navigate to="/login" replace />;
+    const [authorised,setAuthorised] = useState<boolean>();
+    async function checkAuth() {
+        try{
+
+            const response = await fetch("https://localhost:7295/auth/check",{
+                method : "Get",
+                credentials : "include",
+            });
+            if(response.status === 200)
+            {
+                
+                setAuthorised(true);
+            }else{
+                setAuthorised(false);
+                
+            }
+        }catch(error)
+        {
+            console.log("an exception has occured when trying to contact the api: " + error);
+        }
+    }
+    useEffect( ()=>{
+        checkAuth();
+    },[]);
+    if(authorised===true)
+    {
+        console.log("authroised");
+        return <>{children}</>
+    }else if(authorised === false){
+        return <Navigate to="/login" replace/>
+    }
+
 };
