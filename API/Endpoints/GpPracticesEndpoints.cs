8bc97c0a40e3c3e73fc389c1351d20cd0b91d3cf
akbar <moakbar202@gmail.com>
2025-05-21 11:28:12 +0100

    gp endpoints seperated

---
+++ API/Endpoints/GpPracticesEndpoints.cs
@@ -0,0 +1,30 @@
+public static class GpPracticesEndpoints
+{
+    public static void MapGpPracticesEndpoints(this IEndpointRouteBuilder routes)
+    {
+        routes.MapPost("/gpSurgeries",async (GpPractice gp,IGpPracticeService _gpService) =>{
+            try
+            {
+                await _gpService.Add(gp);
+                return Results.Ok(gp);
+            }
+            catch (Exception error)
+            {
+            return Results.BadRequest(new { message = error.Message });
+            }
+        });
+
+        routes.MapGet("/gpSurgeries", async (IGpPracticeService _gpService) =>
+        {
+            try
+            {
+                var results = await _gpService.Get();
+                return Results.Ok(results);
+            }
+            catch (Exception error)
+            {
+                return Results.BadRequest(new { message = error.Message });
+            }
+        });
+    }
+}
